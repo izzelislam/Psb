1f5f3b2fb87d48a7f394979ffb1b25c9576c1672
izzelislam <bangfkr002@gmail.com>
2021-05-31 19:50:56 +0700

    ds

--- resources/views/admin/jadwal/create.blade.php
+++ resources/views/admin/jadwal/create.blade.php
@@ -1,76 +1,20 @@
-@php
-    $action='';
-    if (isset($data)) {
-      $action="/admin/informasi/{$data->id} ";
-    }else{
-      $action="/admin/informasi";
-    }
-
-    $cek=isset($data);
-@endphp
-
-@push('end-style')
-    <link rel="stylesheet" href="{{ asset('stisla/node_modules/select2/dist/css/select2.min.css') }}">
-@endpush
-<div class="container">
-  <h6>{{ $cek ? 'Edit' :'Buat' }} data Q&A</h6>
-</div>
-<form action={{ $action }} method="POST" enctype="multipart/form-data">
-  @csrf
-  @if ($cek)
-      @method('PUT')
-  @endif
-  <div class="card-body">
-    <div class="form-group">
-      <h6>Gambar</h6>
-      <div class="col-sm-6">
-        <img src="{{ $cek ?  Storage::url($data->gambar) : url('https://placehold.it/100x100') }}" id="preview" class="img-thumbnail" style="width: 200px;height:200pxpx;">
-      </div>
-      <input type="file" name="gambar" class="file d-none" accept="image/*">
-      <div class="input-group my-3">
-        <input type="text" class="form-control col-sm-2 ml-3" disabled placeholder="Upload File" id="file">
-        <div class="input-group-append">
-          <button type="button" class="browse btn btn-primary">Browse...</button>
-        </div>
-      </div>
-    </div>
-    <div class="form-group">
-      <label>Judul</label>
-      <div class="input-group">
-        <input type="text" name="title" class="form-control phone-number" value="{{ $cek ? $data->title :'' }}">
-      </div>
-    </div>
-    <div class="form-group">
-      <label>isi</label>
-      <div class="input-group">
-       <textarea name="isi" class="w-100 form-control" style="height: 250px;">
-        {{ $cek ? $data->isi :'' }}
-       </textarea>
-      </div>
-    </div>
-
-    <button type="submit" class="btn btn-primary btn-sm btn-icon icon-left"> <i class="fas fa-save"></i> Kirimkan</button>
-  </div>
-</form>
-@push('end-script')
-    <script src="{{ asset('stisla/node_modules/select2/dist/js/select2.full.min.js') }}"></script>
-@endpush
-
-<script>
-  $(document).on("click", ".browse", function() {
-    var file = $(this).parents().find(".file");
-    file.trigger("click");
-  });
-  $('input[type="file"]').change(function(e) {
-    var fileName = e.target.files[0].name;
-    $("#file").val(fileName);
-
-    var reader = new FileReader();
-    reader.onload = function(e) {
-      // get loaded data and render thumbnail.
-      document.getElementById("preview").src = e.target.result;
-    };
-    // read the image file as a data URL.
-    reader.readAsDataURL(this.files[0]);
-  });
-</script>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="UTF-8">
+  <title>Summernote</title>
+  <link href="https://stackpath.bootstrapcdn.com/bootstrap/3.4.1/css/bootstrap.min.css" rel="stylesheet">
+  <script src="https://code.jquery.com/jquery-3.5.1.min.js"></script>
+  <script src="https://stackpath.bootstrapcdn.com/bootstrap/3.4.1/js/bootstrap.min.js"></script>
+  <link href="https://cdn.jsdelivr.net/npm/summernote@0.8.18/dist/summernote.min.css" rel="stylesheet">
+  <script src="https://cdn.jsdelivr.net/npm/summernote@0.8.18/dist/summernote.min.js"></script>
+</head>
+<body>
+  <div id="summernote"><p>Hello Summernote</p></div>
+  <script>
+    $(document).ready(function() {
+        $('#summernote').summernote();
+    });
+  </script>
+</body>
+</html>
--- resources/views/front/main/index.blade.php
+++ resources/views/front/main/index.blade.php
@@ -36,23 +36,25 @@
 
                 </div>
                 <div class="row mt-5">
+
                 <div class="col m-auto ">
                     <div class="d-none d-md-block">
-                        <img src="{{ asset('front/assets/img/book.png') }}" class="book" style="width: 45%;
-                        margin-bottom: -70px;" alt="" data-aos="zoom-in-up" data-aos-delay="1500" data-aos-duration="1000" />
+                        {{-- <img src="{{ asset('front/assets/img/book.png') }}" class="book" style="width: 45%;
+                        margin-bottom: -70px;" alt="" data-aos="zoom-in-up" data-aos-delay="1500" data-aos-duration="1000" /> --}}
+                        <iframe class="book" width="560" height="315" src="https://www.youtube.com/embed/Aa4oPlkvxMY" title="YouTube video player"  frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
                     </div>
                     <div class="d-sm-block d-md-none">
-                        <img src="{{ asset('front/assets/img/book.png') }}" class="book" style="width: 80%;
-                        margin-bottom: -70px;" alt="" data-aos="zoom-in-up" data-aos-delay="1500" data-aos-duration="1000" />
+                        <iframe class="book" width="295" height="172" src="https://www.youtube.com/embed/Aa4oPlkvxMY" title="YouTube video player"  frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
                     </div>
                 </div>
+
                 </div>
             </div>
             </div>
         </div>
         </div>
-        <div class="container my-5 py-5 px-5">
-            <div class="row justify-content-between">
+        <div class="container my-5 py-5 px-5 ">
+            <div class="row justify-content-between card-info">
                 <div class="col-md-3 col-sm-12 text-light text-center p-3 mb-2 card-1 shadow" data-aos="fade-up" data-aos-delay="50" data-aos-duration="1000">
                     <i style="font-size: 55px" class="mt-2 fa fa-user-graduate"></i>
                     <p><h5>Jumlah Pendaftar</h5>
