9a0712fbbc36eef800b056f996c7f242df84dc91
izzelislam <bangfkr002@gmail.com>
2021-06-05 15:05:13 +0700

    wkwk

--- resources/views/front/main/index.blade.php
+++ resources/views/front/main/index.blade.php
@@ -44,7 +44,7 @@
                         <iframe class="book" width="560" height="315" src="https://www.youtube.com/embed/Aa4oPlkvxMY" title="YouTube video player"  frameborder="0" allow="accelerometer; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
                     </div>
                     <div class="d-sm-block d-md-none">
-                        <iframe class="book" width="295" height="172" src="https://www.youtube.com/embed/Aa4oPlkvxMY?&autoplay=1" title="YouTube video player"  frameborder="0" allow="accelerometer;clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
+                        <iframe class="book" width="295" height="172" src="https://www.youtube.com/embed/Aa4oPlkvxMY" title="YouTube video player"  frameborder="0" allow="accelerometer;clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
                     </div>
                 </div>
 
