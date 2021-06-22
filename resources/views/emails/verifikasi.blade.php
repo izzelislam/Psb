1d06cbfc5aabea4a4df1d5f78667a8dfd285cdcb
izzelislam <bangfkr002@gmail.com>
2021-06-22 17:08:01 +0700

    masnd

--- resources/views/emails/verifikasi.blade.php
+++ resources/views/emails/verifikasi.blade.php
@@ -7,6 +7,11 @@ Untuk melakukan reset password silahkan klik tombol dibawah ini
 Reset Password
 @endcomponent
 
+atau klik dibawah ini jika tombol di atas tidak berfungsi
+<small>
+  <a href="{{ route('getPassword', $user['token']) }}">{{ route('getPassword', $user['token']) }}</a>
+</small>
+
 Terimakasih,<br>
 Admin
 @endcomponent
