f58c34f331a2c8c6cd29c911fdf2ff362544e8ce
izzelislam <bangfkr002@gmail.com>
2021-06-22 16:47:07 +0700

    masnd

--- resources/views/emails/verifikasi.blade.php
+++ resources/views/emails/verifikasi.blade.php
@@ -1,28 +1,9 @@
-{{-- @component('mail::message')
-<h1>Hallo !</h1>
-Silahkan Klik tombol dibawah ini untuk melakukan ResetPassword anda.
-
-@component('mail::button', ['url' => url('/reset-password', $user['token'])])
-Reset password
-@endcomponent
-
-atau klik link di bawah ini
-<br>
-
-
-Terimakasih,<br>
-Admin
-@endcomponent --}}
-
-
-{{-- <a href="{{ route('getPassword', $user['token']) }}">Reset Password</a> --}}
-
 @component('mail::message')
 # hello
 
 Silahkan Klik tombol dibawah ini untuk melakukan ResetPassword anda.
 
-@component('mail::button', ['url' => url('/reset-password', $user['token'])])
+@component('mail::button', ['url' => ''])
 Reset Password
 @endcomponent
 
