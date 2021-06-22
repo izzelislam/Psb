3807743f0c306587ccf860d8bb886d61bef5ae0c
izzelislam <bangfkr002@gmail.com>
2021-06-22 16:25:06 +0700

    ek

--- resources/views/vendor/mail/html/header.blade.php
+++ resources/views/vendor/mail/html/header.blade.php
@@ -2,7 +2,7 @@
 <td class="header">
 <a href="{{ $url }}" style="display: inline-block;">
 @if (trim($slot) === 'Laravel')
-<img src="https://pondokinformatika.com/wp-content/uploads/2016/10/PONDOK-IT-ALMADINAH-PNG-copy.png" style="width: 100%;" class="logo" alt="Laravel Logo">
+<img src="https://pondokinformatika.com/wp-content/uploads/2016/10/PONDOK-IT-ALMADINAH-PNG-copy.png" style="width: 100px;" class="logo" alt="Laravel Logo">
 @else
 {{ $slot }}
 @endif
